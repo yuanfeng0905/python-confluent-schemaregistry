17b831dac29830ab31fe947a0cc1940f37a4c288
Arthur <yuanfeng0905@gmail.com>
2019-08-21 20:34:31 +0800

    add info

--- confluent/schemaregistry/client/CachedSchemaRegistryClient.py
+++ confluent/schemaregistry/client/CachedSchemaRegistryClient.py
@@ -4,12 +4,9 @@ import urllib2
 import json
 import sys
 import hashlib
-import logging
 
 from . import ClientError, VALID_LEVELS
 from ..serializers import Util
-
-LOGGER = logging.getLogger(__name__)
 
 # Common accept header sent
 ACCEPT_HDR="application/vnd.schemaregistry.v1+json, application/vnd.schemaregistry+json, application/json"
@@ -111,14 +108,11 @@ class CachedSchemaRegistryClient(object):
         """
         # 计算schema hash
         avro_schema_hash = self._hash(avro_schema)
-        LOGGER.info('[avro register] subject={}, hash={}'.format(subject, avro_schema_hash))
         schemas_to_id = self.subject_to_schema_ids.get(subject, { })
         schema_id = schemas_to_id.get(avro_schema_hash, -1)
         if schema_id != -1:
-            LOGGER.info('[avro register] schema={}'.format(self.get_by_id(schema_id).to_json()))
             return schema_id
 
-        LOGGER.info('[avro register] schema={}'.format(avro_schema.to_json()))
         # send it up
         url = '/'.join([self.url,'subjects',subject,'versions'])
         # body is { schema : json_string }
